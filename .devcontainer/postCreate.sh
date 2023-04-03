29b771330d6d86ffb51623a2c5c16aa431ea0312
Erik Whitesides <eklevjer@gmail.com>
2023-04-03 13:56:31 +0000

    rm starship and just do newline on bash prompt

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+appended_line='PS1="$PS1\n> "'
+echo $appended_line >> ~/.bashrc
--- .devcontainer/postStart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#install starship,add it to bash,set config
-echo 'eval "$(starship init bash)"' >> ~/.bashrc
-mkdir -p ~/.config
-cp ./.devcontainer/starship.toml ~/.config/starship.toml
